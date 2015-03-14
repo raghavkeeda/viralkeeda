56d9cec0f2a28379b189d553202cffd59af89ece
Raghav Jalan <raghav@viralkeeda.com>
2015-03-15 01:35:39 +0530

    salt

--- wp-config.php
+++ wp-config.php
@@ -31,14 +31,14 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '%-~iFXvl%zzN-Ti|@-#[Y$oYvJe+PB,8<X6Q%fd}OHV1dc+{qcKtPV=fv|zA,:eD');
+define('SECURE_AUTH_KEY',  'H|d^kji2e>*%91Jotebc+TeJo>a9nDmr[,O1_j{wY+.)apCnl6&O&B%gz3-%H_Ax');
+define('LOGGED_IN_KEY',    'k|s~,^8:DsoYPk|fv:7`r|~vQ<2Bv8XG}7Y^8z+Y58EW6>gq:*rMP/SIM,J>!sH%');
+define('NONCE_KEY',        '.,jsKQH RM`6`75|,?a=A+ts2v6tjO0=/bl+A0V#6bS;?+_.`^1(blC+QdO]yp<%');
+define('AUTH_SALT',        'qY6P_UT-YP3&=+Vo:Q/h<<rNmT+BO$P*d=X7BK}H+ J|2uq/<c&<!6cH~E~M6K3e');
+define('SECURE_AUTH_SALT', '#=nd/a%2Ol&ZG)E:08(qtLf=zSSzpX+u^`&|/-Yuazm#+KaDC5+vc[his*hM<2G6');
+define('LOGGED_IN_SALT',   '4KT;+yAa~0~# XnZMXmaRlgAIH<R+M%!!0`oE6lK*O3Kg*nmmeA+Dr3L]`vk=W8l');
+define('NONCE_SALT',       '}Z#+7J]jo{!1vi))B~OqAQ~QO{sj?7BJJ=ty=X`#%emEfPNmM6Lc78I@lV9:^,y<');
 
 // ==============================================================
 // Table prefix
